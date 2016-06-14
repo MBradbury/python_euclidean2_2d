5001d2fb6e5742d7b9f12958ecec73c09138839e
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-06-14 11:07:07 +0100

    Add classifiers and url

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup, Extension
 setup(name="euclidean", version="1.0.2",
-	  description="Provides a fast implementation of euclidean distance",
-	  author="Matthew Bradbury",
-	  license="MIT",
-      ext_modules=[Extension("euclidean", ["src/euclidean_module.c"])])
+      description="Provides a fast implementation of euclidean distance",
+      author="Matthew Bradbury",
+      license="MIT",
+      url="https://github.com/MBradbury/python_euclidean2_2d",
+      ext_modules=[Extension("euclidean", ["src/euclidean_module.c"])],
+      classifiers=["Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 3"]
+)
