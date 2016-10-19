d93ec15161cb99dbe40c6d27779df59da653a086
Matthew Bradbury <matt-bradbury@live.co.uk>
2016-10-19 13:49:28 +0100

    Specify numpy header include directory

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup, Extension
-setup(name="euclidean", version="1.0.2",
+
+import numpy as np
+
+setup(name="euclidean", version="1.0.3",
       description="Provides a fast implementation of euclidean distance",
       author="Matthew Bradbury",
       license="MIT",
       url="https://github.com/MBradbury/python_euclidean2_2d",
       ext_modules=[Extension("euclidean", ["src/euclidean_module.c"])],
+      include_dirs=[np.get_include()],
       classifiers=["Programming Language :: Python :: 2",
                    "Programming Language :: Python :: 3",
                    "Programming Language :: Python :: Implementation :: CPython"]
